fe20b957e01afd2c256446472b1d2d394b35c46b
SohZHong <sohzhehong09@gmail.com>
2025-02-15 22:33:44 +0800

    added sample contract

--- contract/src/Counter.sol
+++ contract/src/Counter.sol
@@ -4,11 +4,23 @@ pragma solidity ^0.8.13;
 contract Counter {
     uint256 public number;
 
-    function setNumber(uint256 newNumber) public {
-        number = newNumber;
+    event NumberSet(address indexed user, uint256 num, uint256 timestamp);
+    event CounterIncremented(address indexed user, uint256 previousNum, uint256 timestamp);
+
+    constructor(uint256 _initialNumber) {
+        number = _initialNumber;
+    }
+
+    function setNumber(uint256 _number) public {
+        number = _number;
+
+        emit NumberSet(msg.sender, _number, block.timestamp);
     }
 
     function increment() public {
+        uint256 previousNum = number;
         number++;
+
+        emit CounterIncremented(msg.sender, previousNum, block.timestamp);
     }
 }
