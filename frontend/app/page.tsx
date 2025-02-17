c6ea3c812ab096fcabf4307bee8483e2f7e78b95
SohZHong <sohzhehong09@gmail.com>
2025-02-17 14:57:34 +0800

    Added placeholders for participants

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,24 +1,29 @@
 'use client';
 
-import { fetchData } from '@/utils/getRequestData';
+import { fetchAllData, fetchUserData } from '@/utils/getRequestData';
 import { useQuery } from '@tanstack/react-query';
 import { useState } from 'react';
-
-const url = 'INSERT URL HERE';
 
 export default function HomePage() {
   const [searchQuery, setSearchQuery] = useState<string>('');
 
-  const { data } = useQuery({
-    queryKey: ['data', searchQuery],
-    queryFn: () => fetchData(searchQuery),
-    enabled: !!searchQuery,
+  // Fetch all data
+  const { data: allData } = useQuery({
+    queryKey: ['data'],
+    queryFn: () => fetchAllData(),
   });
+
+  // Fetch specific user data
+  // const { data } = useQuery({
+  //   queryKey: ['data', searchQuery],
+  //   queryFn: () => fetchUserData(searchQuery),
+  //   enabled: !!searchQuery,
+  // });
   return (
     // Neat! Serialization is now as easy as passing props.
     <div className='grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]'>
       <main className='flex flex-col gap-8 row-start-2 items-center sm:items-start'>
-        <div className='border border-lightgrey p-4 rounded-full flex items-center justify-center gap-2 min-w-full'>
+        {/* <div className='border border-lightgrey p-4 rounded-full flex items-center justify-center gap-2 min-w-full'>
           <input
             type='text'
             className='p-2 w-full outline-none bg-transparent font-bold'
@@ -26,8 +31,9 @@ export default function HomePage() {
             value={searchQuery}
             onChange={(e) => setSearchQuery(e.target.value)}
           />
-        </div>
-        {data && <pre>{JSON.stringify(data.user, null, 2)}</pre>}
+        </div> */}
+        {allData && <pre>{JSON.stringify(allData, null, 2)}</pre>}
+        {/* {data && <pre>{JSON.stringify(data.user, null, 2)}</pre>} */}
       </main>
     </div>
   );
--- frontend/utils/getRequestData.tsx
+++ frontend/utils/getRequestData.tsx
@@ -1,10 +1,24 @@
 import { gql, request } from 'graphql-request';
 
-const url = 'https://api.studio.thegraph.com/query/90479/defi-analysis/v0.0.2';
+const url = 'INSERT ENDPOINT URL HERE';
 
-const SEARCH_QUERY = gql`INSERT QUERY HERE`;
+const SEARCH_QUERY_1 = gql`INSERT QUERY HERE`;
+const SEARCH_QUERY_2 = gql`INSERT QUERY HERE`;
 
-export async function fetchData(searchQuery: string): Promise<any> {
+export async function fetchAllData(): Promise<any> {
+  try {
+    const response = await request(url, SEARCH_QUERY_1);
+    if (!response) {
+      throw new Error('Data not found');
+    }
+    return response;
+  } catch (error) {
+    console.error('GraphQL Request Error:', error);
+    throw error;
+  }
+}
+
+export async function fetchUserData(searchQuery: string): Promise<any> {
   // Return empty
   if (!searchQuery) {
     console.warn('fetchData: searchQuery is empty, returning null');
@@ -12,7 +26,7 @@ export async function fetchData(searchQuery: string): Promise<any> {
   }
 
   try {
-    const response = await request(url, SEARCH_QUERY, {
+    const response = await request(url, SEARCH_QUERY_2, {
       search: searchQuery,
     });
     if (!response) {
