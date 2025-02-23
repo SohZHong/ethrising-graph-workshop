f3c3b734e693ff5e659211ff21fe2c107a9ee8a4
SohZHong <sohzhehong09@gmail.com>
2025-02-23 17:38:33 +0800

    Initialize subgraph

---
+++ counter-test/src/counter.ts
@@ -0,0 +1,33 @@
+import {
+  CounterIncremented as CounterIncrementedEvent,
+  NumberSet as NumberSetEvent
+} from "../generated/Counter/Counter"
+import { CounterIncremented, NumberSet } from "../generated/schema"
+
+export function handleCounterIncremented(event: CounterIncrementedEvent): void {
+  let entity = new CounterIncremented(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
+  entity.user = event.params.user
+  entity.previousNum = event.params.previousNum
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+}
+
+export function handleNumberSet(event: NumberSetEvent): void {
+  let entity = new NumberSet(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
+  entity.user = event.params.user
+  entity.num = event.params.num
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+}
